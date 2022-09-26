a41be8fa2abb8f6b6efd5c73ddec32a65b48697a
Leverow <info.enjoymedia@mail.ru>
2022-09-26 14:21:35 +0500

    AppUser, Article, Identity Provider

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,12 @@
+#pragma warning disable
+using Blog.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
-    
+    public DbSet<Article> Articles { get; set; }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
 }
